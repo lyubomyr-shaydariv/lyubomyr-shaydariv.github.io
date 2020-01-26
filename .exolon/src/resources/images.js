7e55b39ef75e030cb3bc14b56a75b1595261fab6
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2020-01-26 13:45:35 +0200

    exolon

---
+++ .exolon/src/resources/images.js
@@ -0,0 +1,322 @@
+define(function () {
+  
+  var images = [
+    
+    {
+      name: "loading_bg",
+      type: "image",
+      src: "images/loading_bg.gif",
+    },
+    
+    // fonts
+    {
+      name: "font_white",
+      type: "image",
+      src: "images/font_white.gif",
+    },
+    {
+      name: "font_cyan",
+      type: "image",
+      src: "images/font_cyan.gif",
+    },
+    {
+      name: "font_green",
+      type: "image",
+      src: "images/font_green.gif",
+    },
+    {
+      name: "font_purple",
+      type: "image",
+      src: "images/font_purple.gif",
+    },
+    {
+      name: "font_yellow",
+      type: "image",
+      src: "images/font_yellow.gif",
+    },
+    {
+      name: "font_red",
+      type: "image",
+      src: "images/font_red.gif",
+    },
+    
+    // tiles
+    {
+      name: "metatiles",
+      type: "image",
+      src: "images/metatiles.gif",
+    },
+    {
+      name: "tiles",
+      type: "image",
+      src: "images/tiles.gif",
+    },
+    {
+      name: "rocks",
+      type: "image",
+      src: "images/rocks.gif",
+    },
+    {
+      name: "ship",
+      type: "image",
+      src: "images/ship.gif",
+    },
+    {
+      name: "gate",
+      type: "image",
+      src: "images/gate.gif",
+    },
+    
+    // entities
+    {
+      name: "star",
+      type: "image",
+      src: "images/star.gif",
+    },
+    {
+      name: "vitorc",
+      type: "image",
+      src: "images/vitorc.gif",
+    },
+    {
+      name: "vitorc2",
+      type: "image",
+      src: "images/vitorc2.gif",
+    },
+    {
+      name: "blaster_bullet",
+      type: "image",
+      src: "images/blaster_bullet.gif",
+    },
+    {
+      name: "blaster_explosion",
+      type: "image",
+      src: "images/blaster_explosion.gif",
+    },
+    {
+      name: "grenade",
+      type: "image",
+      src: "images/grenade.gif",
+    },
+    {
+      name: "grenade_trace",
+      type: "image",
+      src: "images/grenade_trace.gif",
+    },
+    {
+      name: "grenade_pack",
+      type: "image",
+      src: "images/grenade_pack.gif",
+    },
+    {
+      name: "ammo_pack",
+      type: "image",
+      src: "images/ammo_pack.gif",
+    },
+    {
+      name: "turret",
+      type: "image",
+      src: "images/turret.gif",
+    },
+    {
+      name: "turret_body",
+      type: "image",
+      src: "images/turret_body.gif",
+    },
+    {
+      name: "turret_tube",
+      type: "image",
+      src: "images/turret_tube.gif",
+    },
+    {
+      name: "turret_bullet",
+      type: "image",
+      src: "images/turret_bullet.gif",
+    },
+    {
+      name: "explosion_particle",
+      type: "image",
+      src: "images/explosion_particle.gif",
+    },
+    {
+      name: "cocoon",
+      type: "image",
+      src: "images/cocoon.gif",
+    },
+    {
+      name: "radar",
+      type: "image",
+      src: "images/radar.gif",
+    },
+    {
+      name: "rocket",
+      type: "image",
+      src: "images/rocket.gif",
+    },
+    {
+      name: "ship_fire",
+      type: "image",
+      src: "images/ship_fire.gif",
+    },
+    {
+      name: "light",
+      type: "image",
+      src: "images/light.gif",
+    },
+    {
+      name: "teleport",
+      type: "image",
+      src: "images/teleport.gif",
+    },
+    {
+      name: "teleport_flash",
+      type: "image",
+      src: "images/teleport_flash.gif",
+    },
+    {
+      name: "piston",
+      type: "image",
+      src: "images/piston.gif",
+    },
+    {
+      name: "harbringer",
+      type: "image",
+      src: "images/harbringer.gif",
+    },
+    {
+      name: "circular_explosion",
+      type: "image",
+      src: "images/circular_explosion.gif",
+    },
+    {
+      name: "bubble",
+      type: "image",
+      src: "images/bubble.gif",
+    },
+    {
+      name: "incubator",
+      type: "image",
+      src: "images/incubator.gif",
+    },
+    {
+      name: "egg",
+      type: "image",
+      src: "images/egg.png",
+    },
+    {
+      name: "double_launcher",
+      type: "image",
+      src: "images/double_launcher.gif",
+    },
+    {
+      name: "double_launcher_bullet",
+      type: "image",
+      src: "images/double_launcher_bullet.gif",
+    },
+    {
+      name: "mine",
+      type: "image",
+      src: "images/mine.gif",
+    },
+    {
+      name: "mine_fire",
+      type: "image",
+      src: "images/mine_fire.gif",
+    },
+    {
+      name: "missile_guidance",
+      type: "image",
+      src: "images/missile_guidance.gif",
+    },
+    {
+      name: "missile",
+      type: "image",
+      src: "images/missile.gif",
+    },
+    {
+      name: "interceptor",
+      type: "image",
+      src: "images/interceptor.gif",
+    },
+    {
+      name: "jellyfish",
+      type: "image",
+      src: "images/jellyfish.gif",
+    },
+    {
+      name: "waggon",
+      type: "image",
+      src: "images/waggon.gif",
+    },
+    {
+      name: "combined_launcher_top",
+      type: "image",
+      src: "images/combined_launcher_top.gif",
+    },
+    {
+      name: "combined_launcher_bottom",
+      type: "image",
+      src: "images/combined_launcher_bottom.gif",
+    },
+    {
+      name: "square_light",
+      type: "image",
+      src: "images/square_light.gif",
+    },
+    {
+      name: "discharge",
+      type: "image",
+      src: "images/discharge.gif",
+    },
+    {
+      name: "level_complete_window",
+      type: "image",
+      src: "images/level_complete_window.gif",
+    },
+    {
+      name: "fungus",
+      type: "image",
+      src: "images/fungus.gif",
+    },
+    {
+      name: "louse",
+      type: "image",
+      src: "images/louse.gif",
+    },
+    {
+      name: "beam",
+      type: "image",
+      src: "images/beam.gif",
+    },
+    {
+      name: "flasher",
+      type: "image",
+      src: "images/flasher.gif",
+    },
+    {
+      name: "fir",
+      type: "image",
+      src: "images/fir.gif",
+    },
+    
+    // other
+    {
+      name: "title",
+      type: "image",
+      src: "images/title.gif",
+    },
+    {
+      name: "bonus_screen",
+      type: "image",
+      src: "images/bonus_screen.gif",
+    },
+    {
+      name: "arrow",
+      type: "image",
+      src: "images/arrow.gif",
+    },
+  ];
+  
+  return images;
+  
+});
